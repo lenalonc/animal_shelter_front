16f29456ffb529319bcca2adc7a7b4f88db32441
Lena Loncarevic <l.loncarevic-int@levi9.com>
2024-04-14 23:53:56 +0200

    Back button updated

--- src/components/owners/OwnerDetails.js
+++ src/components/owners/OwnerDetails.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import api from "../../api/Api";
+import { Link } from "react-router-dom";
 
 const OwnerDetails = () => {
   const { id } = useParams();
@@ -72,6 +73,24 @@ const OwnerDetails = () => {
 
   return (
     <div className="owner-details-page">
+      <Link to={"/owners"}>
+        <button
+          className="btn btn-primary btn-adopt btn-owner-details btn-back"
+          style={{ width: "100px", alignItems: "center" }}
+        >
+          {/* <img src={arrowLeft} alt="Back" style={{ width: 20 }} /> */}
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="25"
+            height="25"
+            fill="currentColor"
+            class="bi bi-caret-left"
+            viewBox="0 0 16 16"
+          >
+            <path d="M10 12.796V3.204L4.519 8zm-.659.753-5.48-4.796a1 1 0 0 1 0-1.506l5.48-4.796A1 1 0 0 1 11 3.204v9.592a1 1 0 0 1-1.659.753" />
+          </svg>
+        </button>
+      </Link>
       <div className="owner-details">
         <div className="owner-details-header">
           <h2>Owner Details</h2>
